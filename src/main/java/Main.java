20d859bc7aba19d7d47a8c753071b4edefdff437
seanjungmsba <seanjeong1996@gmail.com>
2022-07-26 19:50:28 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,26 @@ import java.util.Scanner;
 import java.util.concurrent.*;
 
 public class Main {
+
+    private final static int NUMBER_OF_THREADS = 1;
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+        System.out.print("Enter a number that is to be checked for its primality: ");
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+            executor.submit(() -> {
+                PrimeLogger primeLogger = new PrimeLogger(num);
+                primeLogger.run();
+            });
         }
+        executor.shutdown();
+        scanner.close();
     }
 }
 
@@ -24,5 +35,20 @@ class PrimeLogger implements Runnable {
     @Override
     public void run() {
         // print num if it is prime
+        boolean flag = false;
+        int i = 2;
+        while (i <= this.num / 2) {
+            // condition for nonprime number
+            if (this.num % i == 0) {
+                flag = true;
+                break;
+            }
+            ++i;
+        }
+        if (!flag)
+            System.out.println(this.num + " is a prime number.");
+        else
+            System.out.println(this.num + " is not a prime number.");
     }
+    
 }
